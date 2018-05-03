ad722500f2f0c848d91b6394849184a5818bc14c
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-05-03 12:06:29 -0300

    Add files via upload

---
+++ GerFlix-master/GerFlix Practico Consginas y Ejercicios/GerFlix 1.0/Serie.c
@@ -0,0 +1,86 @@
+
+#include "Serie.h"
+#include <string.h>
+
+/*
+    int idSerie;
+    char nombre[50];
+    char genero[20];
+    int cantidadTemporadas;
+    int estado;
+
+*/
+
+void inicializarSeriesEstado(eSerie series[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        series[i].estado = 0;
+    }
+}
+
+
+void inicializarSeriesHardCode(eSerie series[])
+{
+    int id[5] = {100,101,102,103,104};
+    char nombre[][50]={"TBBT","TWD","GOT","BB","LCDP"};
+    char genero[][50]={"Comedia","Terror","Suspenso","Policial","Policial"};
+    int cantidad[5] = {10,9,7,5,2};
+
+    int i;
+
+    for(i=0; i<5; i++)
+    {
+        series[i].idSerie=id[i];
+        series[i].cantidadTemporadas=cantidad[i];
+        series[i].estado = 1;
+        strcpy(series[i].nombre, nombre[i]);
+        strcpy(series[i].genero, genero[i]);
+    }
+}
+
+void mostrarListaSeries(eSerie serie[], int t)
+{
+int i;
+    for(i=0; i<t; i++)
+    {
+     if(serie[i].estado==1)
+     {
+       printf("%d %s %s %d\n ", serie[i].idSerie,serie[i].nombre, serie[i].genero, serie[i].cantidadTemporadas);
+     }
+
+    }
+}
+void mostrarSerieconsuUsuario(eSerie serie[], int a, eUsuario usuario[], int b )
+{
+
+   int i;
+   int j;
+
+    for(i=0; i<a; i++)
+    {
+
+        for (j=0+1; j<b; j++ )
+        {
+
+           if(serie.idSerie[i]==usuario.idSerie)
+            {
+             printf("%d %s %s %d %s\n", serie[i].idSerie,serie[i].nombre,serie[i].genero,serie[i].cantidadTemporadas,usuario[j].nombre) ;
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ GerFlix-master/GerFlix Practico Consginas y Ejercicios/GerFlix 1.0/Usuario.c
@@ -0,0 +1,83 @@
+
+#include "Usuario.h"
+
+void inicializarUsuariosEstado(eUsuario usuarios[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        usuarios[i].estado = 0;
+    }
+}
+void inicializarUsuariosHardCode(eUsuario usuarios[])
+{
+
+
+    int id[15] = {1000,1001,1002,1003,1004, 1005,1006,1007,1008,1009, 1010,1011,1012,1013,1014};
+    char nombre[][50]= {"Juan","Maria","Pedro","Vanesa","Jose","Luisa","Laura","Marcelo","German","Victoria","Dafne","Antonela","Gisele","Dario","Ricardo"};
+
+    int serie[15] = {100,100,101,101,102,100,100,103,101,102,103,101,100,100,101};
+
+
+
+    int i;
+
+    for(i=0; i<15; i++)
+    {
+        usuarios[i].idUsuario=id[i];
+        usuarios[i].idSerie=serie[i];
+        usuarios[i].estado = 1;
+        strcpy(usuarios[i].nombre, nombre[i]);
+
+    }
+
+}
+
+void mostrarListaUsuarios(eUsuario usuario[], int t)
+{
+
+    int i;
+    for(i=0; i<t; i++)
+    {
+     if(usuario[i].estado==1)
+     {
+       printf("%d %s %d\n ", usuario[i].idUsuario,usuario[i].nombre, usuario[i].idSerie);
+     }
+
+    }
+}
+
+void mostrarUsuarioConSuSerie(eUsuario usuario[], int c, eSerie serie[], int t)
+{
+    int i;
+    int j;
+
+
+
+
+    for(i=0; i<c; i++)
+    {
+       if(serie[j].estado==1)
+       {
+         printf("%s", serie.nombre);
+
+        for(j=0; j<t;j++)
+        {
+          if(usuario[i].idSerie==serie[j].idSerie)
+          {
+            //printf("%d  %s %d %s %s\n", usuario[i].idUsuario,usuario[i].nombre,usuario[i].idSerie,serie[j].nombre,serie[j].genero );
+
+
+          if(usuario[i].estado==1)
+           {
+             printf("%d  %s, %d %s %s\n", usuario[i].idUsuario,usuario[i].nombre,usuario[i].idSerie,serie[j].nombre,serie[j].genero );
+             break;
+           }
+
+          }
+        }
+       }
+    }
+}
+
